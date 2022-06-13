869061e374d97a59d2ee20756ce1b04fbdf4197c
Chipio Industries <53354220+ChipioIndustries@users.noreply.github.com>
2022-06-12 21:22:18 -0700

    Docs

---
+++ docs/docusaurus.config.js
@@ -0,0 +1,83 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'gopher',
+  tagline: 'documentation',
+  url: 'https://chipioindustries.github.io',
+  baseUrl: '/gopher/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'chipioindustries', // Usually your GitHub org/user name.
+  projectName: 'gopher', // Usually your repo name.
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js')
+        },
+        blog: {
+          showReadingTime: true
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Gopher',
+        logo: {
+          alt: 'Site Logo',
+          src: 'img/logo.png',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'dev-setup',
+            position: 'left',
+            label: 'Get Started',
+          },
+          {
+            href: 'https://github.com/chipioindustries/gopher',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Copyright © ${new Date().getFullYear()} Chipio Industries. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+		additionalLanguages: ['lua'],
+      },
+    }),
+};
+
+module.exports = config;
